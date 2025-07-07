8709c1d8fc6609d5035beb66e6c63ca5d6b7d880
Manjula123-hi <danthulurimanjula@gmail.com>
2025-07-07 12:34:22 +0530

    terraform

--- 20-bastion/main.tf
+++ 20-bastion/main.tf
@@ -3,6 +3,7 @@ resource "aws_instance" "bastion" {
   instance_type = "t3.micro"
   vpc_security_group_ids = [local.bastion_sg_id]
   subnet_id = local.public_subnet_id
+  iam_instance_profile = "TerraformAdmin"
   tags = merge(
     local.common_tags,{
         Name = "${var.project}-${var.environment}-bastion"
